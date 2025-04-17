a88cf2cdef4c6e3ec8a8ea4521acac0d3665df66
bidyut1994 <bbboruah199@gmail.com>
2025-04-17 17:39:30 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <button className="bg-blue-500 text-white p-2 rounded-md">
+        Login with google
+      </button>
+    </div>
+  );
+}
+
+export default App;
